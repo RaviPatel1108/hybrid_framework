25b92f7176df9dc7fed90c1a373bec4e35cad2ac
112726 <ravi1122b@gmail.com>
2023-03-02 11:59:53 +0530

    Header check

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from assertpy import assert_that
+from selenium.webdriver.common.by import By
 
 
 class TestLoginUi:
@@ -11,6 +12,15 @@ class TestLoginUi:
         actual_title = driver.title
         # assert actual_title == "OrangeHRM"
         assert_that("OrangeHRM").is_equal_to(actual_title)
+
+    def test_header(self):
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.implicitly_wait(20)
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+        actual_header = driver.find_element(By.XPATH, "//h5[text()='Login']").text
+        assert_that("Login").is_equal_to(actual_header)
+
 
 
 
