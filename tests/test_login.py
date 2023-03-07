4d81a8c0d699b7e895a92acfa63b5304cc89ec96
112726 <ravi1122b@gmail.com>
2023-03-07 10:46:26 +0530

    multiple data handling with pytest module

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,5 @@
+import pytest
+
 from base.webdriver_listener import WebdriverWrapper
 from assertpy import assert_that
 from selenium.webdriver.common.by import By
@@ -11,12 +13,16 @@ class TestLogin(WebdriverWrapper):
         actual_header = self.driver.find_element(By.XPATH, "//h6[normalize-space()='Dashboard']").text
         assert_that("Dashboard").is_equal_to(actual_header)
 
-    def test_invalid_login(self):
-        self.driver.find_element(By.NAME, "username").send_keys("Admin")
-        self.driver.find_element(By.NAME, "password").send_keys("admin12345")
+    @pytest.mark.parametrize("username, password, expected_error",
+                             [("ravi", "ravi123", "Invalid credentials"),
+                              ("sunny", "sunny123", "Invalid credentials")]
+                             )
+    def test_invalid_login(self, username, password, expected_error):
+        self.driver.find_element(By.NAME, "username").send_keys(username)
+        self.driver.find_element(By.NAME, "password").send_keys(password)
         self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
         actual_error = self.driver.find_element(By.XPATH, "//p[normalize-space()='Invalid credentials']").text
-        assert_that("Invalid credentials").is_equal_to(actual_error)
+        assert_that(expected_error).is_equal_to(actual_error)
 
 
 class TestLoginUi(WebdriverWrapper):
@@ -28,3 +34,9 @@ class TestLoginUi(WebdriverWrapper):
     def test_header(self):
         actual_header = self.driver.find_element(By.XPATH, "//h5[text()='Login']").text
         assert_that("Login").is_equal_to(actual_header)
+
+    def test_login_placeholders(self):
+        actual_login_placeholder = self.driver.find_element(By.NAME, "username").get_attribute("placeholder")
+        actual_password_placeholder = self.driver.find_element(By.NAME, "password").get_attribute("placeholder")
+        assert assert_that("Username").is_equal_to(actual_login_placeholder)
+        assert assert_that("Password").is_equal_to(actual_password_placeholder)
