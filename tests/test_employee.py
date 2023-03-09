5545ad9c0bf2ee2182d05e92159eddd27a6e53a1
112726 <ravi1122b@gmail.com>
2023-03-09 09:03:17 +0530

    Add Employee Assignment of Mar 2,6 and 7

--- tests/test_employee.py
+++ tests/test_employee.py
@@ -1,26 +1,30 @@
+import pytest
 from selenium.webdriver.common.by import By
 from assertpy import assert_that
 from base.webdriver_listener import WebdriverWrapper
+from utilities.data_source import test_add_employee_data
 import time
 
 
 class TestAddEmployee(WebdriverWrapper):
-    def test_add_valid_employee(self):
-        self.driver.find_element(By.NAME, "username").send_keys("Admin")
-        self.driver.find_element(By.NAME, "password").send_keys("admin123")
+
+    @pytest.mark.parametrize("username, password, firstname, middlename, lastname, expected_name1, expected_first_name", test_add_employee_data)
+    def test_add_valid_employee(self, username, password, firstname, middlename, lastname, expected_name1, expected_first_name):
+        self.driver.find_element(By.NAME, "username").send_keys(username)
+        self.driver.find_element(By.NAME, "password").send_keys(password)
         self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
         self.driver.find_element(By.XPATH, "//span[text()='PIM']").click()
         self.driver.find_element(By.XPATH, "//a[text()='Add Employee']").click()
-        self.driver.find_element(By.NAME, "firstName").send_keys("Ravi")
-        self.driver.find_element(By.NAME, "lastName").send_keys("Patel")
+        self.driver.find_element(By.NAME, "firstName").send_keys(firstname)
+        self.driver.find_element(By.NAME, "middleName").send_keys(middlename)
+        self.driver.find_element(By.NAME, "lastName").send_keys(lastname)
         self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
         time.sleep(20)
 
         actual_employee_name = self.driver.find_element(By.XPATH, "//div[contains(@class,'employee-name')]").text
-        assert_that("Ravi Patel").is_equal_to(actual_employee_name)
+        assert_that(expected_name1).is_equal_to(actual_employee_name)
         actual_first_name = self.driver.find_element(By.NAME, "firstName").get_attribute("value")
-        assert_that("Ravi").is_equal_to(actual_first_name)
-        actual_last_name = self.driver.find_element(By.NAME, "lastName").get_attribute("value")
-        assert_that("Patel").is_equal_to(actual_last_name)
+        assert_that(expected_first_name).is_equal_to(actual_first_name)
+
 
 
--- utilities/data_source.py
+++ utilities/data_source.py
@@ -1,2 +1,5 @@
 test_invalid_login_data = [("ravi", "ravi123", "Invalid credentials"),
                            ("sunny", "sunny123", "Invalid credentials")]
+
+test_add_employee_data = [("Admin", "admin123", "John", "J", "Wick", "John Wick", "John"),
+                          ("Admin", "admin123", "Peter", "J", "Wick", "Peter Wick", "Peter")]
