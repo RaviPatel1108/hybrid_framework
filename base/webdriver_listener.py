93b80f9e5d5e94d92a8b692d773dd107e5dc48bc
112726 <ravi1122b@gmail.com>
2023-03-16 11:52:16 +0530

    Json file added

--- base/webdriver_listener.py
+++ base/webdriver_listener.py
@@ -1,11 +1,16 @@
 import pytest
 from selenium import webdriver
+from utilities import read_utils
 
 
 class WebdriverWrapper:
     @pytest.fixture(scope="function", autouse=True)
     def setup(self):
-        self.driver = webdriver.Chrome()
+        browser_name = read_utils.get_value_from_json("../test_data/data.json", "browser")
+        if browser_name == "chrome":
+            self.driver = webdriver.Chrome()
+        elif browser_name == "ff":
+            self.driver = webdriver.firefox
         self.driver.maximize_window()
         self.driver.implicitly_wait(20)
         self.driver.get("https://opensource-demo.orangehrmlive.com/")
